c8350cfebdcb742313a4f52be1b4f7abe4da8ec7
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2023-08-04 20:31:08 +0500

    done with the frontend

--- script.js
+++ script.js
@@ -1,39 +1,16 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=';
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '58ce39cc1amsha4354a3355d61d5p15a347jsn0f6c11dcc7da',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-    }
-};
-
-var cityName = document.getElementById('cityName');
-
-function getWeather(city){
+var section_weather_search = document.getElementById('weather-search');
+var section_weather_info = document.getElementById('Weather-info');
+const cityName = document.getElementById('cityName');
 
 
-fetch(url + cityName.value, options)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-       // cloud_pct = response.cloud_pct;
-        document.getElementById('temp').innerHTML = response.temp;
-        // feels_like = response.feels_like;
-        // humidity = response.humidity;
-        // min_temp = response.min_temp;
-        // max_temp = response.max_temp;
-        // wind_speed = response.wind_speed;
-        // wind_degrees = response.wind_degrees;
-        // sunrise = response.sunrise;
-        // sunset = response.sunset;
-    })
-    .catch(err => console.error(err));
+var search_location_btn = document.getElementById('search-location-btn').addEventListener('click', () => {
+    section_weather_info.classList.remove('d-none')
+    section_weather_search.classList.add('d-none')
+});
 
-}
 
-submitButton.addEventListener('click', ()=>{
-    if (cityName === '') return alert("Please enter a valid City Name");
-    else{
-        getWeather(cityName);
-    }
-})
+let backbutton = document.getElementById('back-button').addEventListener('click', () => {
+    section_weather_info.classList.add('d-none')
+    section_weather_search.classList.remove('d-none')
+});
+
